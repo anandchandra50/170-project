60a34adeaeb8087bac5937ca880feaa0fe4bb6b0
Anand Chandra <anandchandra50@gmail.com>
2020-05-01 01:03:10 -0700

    uploading proj

--- solver.py
+++ solver.py
@@ -1,8 +1,23 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance
+from os import listdir
+from os.path import isfile, join
 import sys
 
+# return a list of tuple edges connected to a node
+def get_edgelist(edges_by_node, node):
+    all_edges = []
+    for other_vertex in edges_by_node[node]:
+        all_edges.append((node, other_vertex))
+    return all_edges
+
+def get_connecting_edges(nodes, new_node, edges_by_node):
+    current_nodes_that_connect_to_new = set(edges_by_node[new_node]).intersection(nodes)
+    all_edges = []
+    for current in current_nodes_that_connect_to_new:
+        all_edges.append((new_node, current))
+    return all_edges
 
 def solve(G):
     """
@@ -12,10 +27,79 @@ def solve(G):
     Returns:
         T: networkx.Graph
     """
+    T = nx.minimum_spanning_tree(G)
+    node_pairwise_distance = {} # node number to total distance from all other nodes
 
+    path_lengths = dict(nx.all_pairs_dijkstra_path_length(G))
+    for node in G.nodes:
+        node_pairwise_distance[node] = sum(path_lengths[node][n] for n in G.nodes)
+    node_pairwise_distance = {k: v for k, v in sorted(node_pairwise_distance.items(), key=lambda item: item[1], reverse=True)}
+
+    for node in node_pairwise_distance: # descending order, remove nodes with highest distances when possible
+        t_copy = nx.Graph()
+        t_copy.add_nodes_from(T.nodes)
+        t_copy.add_edges_from(T.edges)
+        t_copy.remove_node(node)
+        if len(list(t_copy.nodes)) == 0:
+            continue
+        if is_valid_network(G, t_copy):
+            T.remove_node(node)
+    return T
     # TODO: your code here!
-    pass
-
+    # create T
+    # T = nx.Graph()
+    #
+    # edges_by_node = {} # node to list of other nodes (goes both ways), represents edge matrix.
+    # for node in G.nodes:
+    #     edges_by_node[node] = []
+    # for edge in G.edges:
+    #     edges_by_node[edge[0]].append(edge[1])
+    #     edges_by_node[edge[1]].append(edge[0])
+    #
+    # node_pairwise_distance = {} # node number to total distance from all other nodes
+    #
+    # path_lengths = dict(nx.all_pairs_dijkstra_path_length(G))
+    # for node in G.nodes:
+    #     node_pairwise_distance[node] = sum(path_lengths[node][n] for n in G.nodes)
+    # node_pairwise_distance = {k: v for k, v in sorted(node_pairwise_distance.items(), key=lambda item: item[1])}
+    # relevant_nodes = [] # (node, edge) keep track of neighboring nodes that we can add (ensure we build a tree)
+    # print(node_pairwise_distance)
+    # # begin the algorithm
+    # first_node = list(node_pairwise_distance.keys())[0]
+    # T.add_node(first_node)
+    # relevant_nodes = relevant_nodes + list(G.neighbors(first_node))
+    # relevant_nodes.sort(key=lambda item: node_pairwise_distance[item])
+    # print(G.nodes)
+    # print(G.edges)
+    # print('starting: {}'.format(T.nodes))
+    # # now loop
+    # while True:
+    #     print('running {}'.format(relevant_nodes))
+    #     next_node = relevant_nodes.pop(0)
+    #     # T.add_node(next_node)
+    #     new_edges = get_connecting_edges(T.nodes, next_node, edges_by_node)
+    #     print(new_edges[0])
+    #     T.add_edge(*new_edges[0])
+    #     print('nodes: {}'.format(T.nodes))
+    #     if not nx.is_tree(T):
+    #         print('no longer tree')
+    #         T.remove_node(next_node)
+    #         if len(relevant_nodes) == 0:
+    #             return T
+    #         relevant_nodes.append(next_node)
+    #         continue
+    #
+    #     # if is_valid_network(G, T) and # is valid, but adding the next node increases the numbers
+    #     if not is_valid_network(G, T): # still a tree, but doesn't dominate
+    #         # keep this next vertex
+    #         # add the neighboring nodes
+    #         relevant_nodes = relevant_nodes + list(G.neighbors(node))
+    #         relevant_nodes = list(set(relevant_nodes))
+    #         relevant_nodes.sort(key=lambda item: node_pairwise_distance[item])
+    #         continue
+    #     else: # is a tree and dominates, optimize this later
+    #         return T
+    # return T
 
 # Here's an example of how to run your solver.
 
@@ -28,4 +112,14 @@ def solve(G):
 #     T = solve(G)
 #     assert is_valid_network(G, T)
 #     print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-#     write_output_file(T, 'out/test.out')
+#     write_output_file(T, 'outputs/test.out')
+
+if __name__ == '__main__':
+    onlyfiles = [f for f in listdir('inputs') if isfile(join('inputs', f))]
+    for f in onlyfiles:
+        file_name = f.split('.')[0]
+        print(file_name)
+        G = read_input_file('inputs/' + file_name + '.in')
+        T = solve(G)
+        assert is_valid_network(G, T)
+        write_output_file(T, 'outputs/' + file_name + '.out')
